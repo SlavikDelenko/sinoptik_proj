a6d2a07daa5f58e5cc8f84d7e6017fbff9bc15ee
fiyzech <svis5474@gmail.com>
2024-01-09 20:13:43 +0200

    added request to API OpenWeather

---
+++ src/hooks/request.tsx
@@ -0,0 +1,48 @@
+import { useState, useEffect } from 'react'; 
+import axios from 'axios'; 
+ 
+interface WeatherData { 
+  main: { 
+    temp: number; 
+  }; 
+  weather: { 
+    description: string; 
+  }[]; 
+} 
+ 
+interface WeatherApiHook { 
+  weatherData: WeatherData | null; 
+  loading: boolean; 
+  error: string | null; 
+} 
+ 
+const useWeatherApi = (city: string): WeatherApiHook => { 
+  const [weatherData, setWeatherData] = useState<WeatherData | null>(null); 
+  const [loading, setLoading] = useState(true); 
+  const [error, setError] = useState<string | null>(null); 
+ 
+  useEffect(() => { 
+    const fetchWeatherData = async () => { 
+      try { 
+        const apiKey = 'be062ae90cf3d3303f85298bf828843e'; 
+        const apiUrl = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;  
+        
+        const response = await axios.get<WeatherData>(apiUrl);  
+        
+        setWeatherData(response.data); 
+        setLoading(false); 
+        setError(null); 
+      } catch (error) { 
+        setLoading(false); 
+        setError('Error fetching weather data'); 
+        setWeatherData(null); 
+      } 
+    }; 
+ 
+    fetchWeatherData(); 
+  }, [city]); 
+ 
+  return { weatherData, loading, error }; 
+}; 
+ 
+export default useWeatherApi;
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App.tsx';
+import ReactDOM from 'react-dom';
+import DisplayWeatherData from './components/Weather';
 import './index.css';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>,
+ReactDOM.render(
+  <React.StrictMode>
+    <DisplayWeatherData city="Kyiv" />
+  </React.StrictMode>,
+  document.getElementById('root')
 );
